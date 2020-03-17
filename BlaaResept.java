5e97d609c3d5fac906ffc24f370c2d4bc7150b67
yuyanf <62013327+yuyanf@users.noreply.github.com>
2020-03-17 15:55:05 +0100

    Ga nytt navn til klasser og oppdatert Lege

---
+++ BlaaResept.java
@@ -0,0 +1,19 @@
+public class BlaaResept extends Resept{
+    double betal = 0.25;
+
+    public BlaaResept(Legemiddel lm, Lege l, int p, int r){
+        super(lm, l, p, r);
+    }
+
+    public String hentFarge(){
+        return "Bla resept";
+    }
+
+    public double prosAaBetale(){
+        return (leg.hentPris() * betal);
+    }
+
+    public String toString(){
+        return("legemiddel: " + leg.hentNavn() + " lege: " + doc.hentNavn() + " pris: " + pasientID + " reit: " + reit + " pris: " + leg.hentPris() + " betal: " + (leg.hentPris() * betal));
+    }
+}
