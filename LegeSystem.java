07d6a41086679b22569f682079130a87ddeb961d
Yllnor13 <62013204+Yllnor13@users.noreply.github.com>
2020-03-22 00:50:00 +0100

    Add files via upload

--- LegeSystem.java
+++ LegeSystem.java
@@ -10,6 +10,7 @@ public class LegeSystem{
 
     public static void main (String[] args){
         File tekst = new File("innlesing.txt");
+        les(tekst);
         meny();
     }
 
@@ -24,13 +25,123 @@ public class LegeSystem{
 
         String lest = leser.nextLine();
         while(leser.hasNextLine()){
-            String[] lestinn = lest.split(" ");
-            if(lestinn[1].compareTo("Pasienter") == 0){
+            String [] tittel = lest.split(" ");
+            if(tittel[1] == "Pasienter"){
                 while(leser.hasNextLine()){
                     lest = leser.nextLine();
+                    if(lest.charAt(0) == '#'){
+                        break;
+                    }
+                    String[] info = lest.split(",");
+                    Pasient pasient = new Pasient(info[0],info[1]);
+                    pasienter.leggTil(pasient);
+                }
+            }
+            else if(tittel[1] == "Legemidler"){
+                while(leser.hasNextLine()){
+                    lest = leser.nextLine();
+                    if(lest.charAt(0) == '#'){
+                        break;
+                    }
+                    String[] info = lest.split(",");
+                    if(info[1] == "narkotisk"){
+                        int styrke = Integer.parseInt(info[4]);
+                        Double pris = Double.parseDouble(info[2]);
+                        Double virkestoff = Double.parseDouble(info[3]);
+                        Narkotisk narkotisk = new Narkotisk(info[0], pris, virkestoff, styrke);
+                        legemidler.leggTil(narkotisk);
+                    }
+                    if(info[1] == "vanedannede"){
+                        int styrke = Integer.parseInt(info[4]);
+                        Double pris = Double.parseDouble(info[2]);
+                        Double virkestoff = Double.parseDouble(info[3]);
+                        Vanedannede vanedannede = new Vanedannede(info[0], pris, virkestoff, styrke);
+                        legemidler.leggTil(vanedannede);
+                    }
+                    if(info[1] == "vanlig"){
+                        Double pris = Double.parseDouble(info[2]);
+                        Double virkestoff = Double.parseDouble(info[3]);
+                        Vanlig vanlig = new Vanlig(info[0], pris, virkestoff);
+                        legemidler.leggTil(vanlig);
+                    }
+                }
+            }
+            else if(tittel[1] == "Leger"){
+                while(leser.hasNextLine()){
+                    lest = leser.nextLine();
+                    if(lest.charAt(0) == '#'){
+                        break;
+                    }
+                    String[] info = lest.split(",");
+                    int id = Integer.parseInt(info[1]);
+                    if(id > 0){
+                        Spesialist spesialist = new Spesialist(info[0], id);
+                        leger.leggTil(spesialist);
+                    }
+                    else{
+                        Lege lege = new Lege(info[0]);
+                        leger.leggTil(lege);
+                    }
+                }
+            }
+            else if(tittel[1] == "Resepter"){
+                while(leser.hasNextLine()){
+                    lest = leser.nextLine();
+                    String[] info = lest.split(",");
+                    Legemiddel nymid = null;
+                    Pasient nypas = null;
+                    Lege nyleg = null;
+                    int middelId = Integer.parseInt(info[0]);
+                    int pasId = Integer.parseInt(info[2]);
+                    
+                    for(Legemiddel i : legemidler){
+                        if(i.hentId() == middelId){
+                            nymid = i;
+                        }
+                    }
+                    for(Lege i : leger){
+                        if(i.hentNavn() == info[1]){
+                            nyleg = i;
+                        }
+                    }
+                    for(Pasient i : pasienter){
+                        if(i.hentId() == pasId){
+                            nypas = i;
+                        }
+                    }
+                    if(info[3] == "hvit"){
+                        int reit = Integer.parseInt(info[4]);
+                        try{
+                            Resept resept = nyleg.skrivHvitResept(nymid, nypas, reit);
+                            resepter.leggTil(resept);
+                        }
+                        catch(UlovligUtskrift e){
+                            System.out.println(e);
+                        }
+                    }
+                    else if(info[3] == "blaa"){
+                        int reit = Integer.parseInt(info[4]);
+                        try{
+                            Resept resept = nyleg.skrivBlaaResept(nymid, nypas, reit);
+                            resepter.leggTil(resept);
+                        }
+                        catch(UlovligUtskrift e){
+                            System.out.println(e);
+                        }
+                    }
+                    else if(info[3] == "p"){
+                        try{
+                            Resept resept = nyleg.skrivPResept(nymid, nypas);
+                            resepter.leggTil(resept);
+                        }
+                        catch(UlovligUtskrift e){
+                            System.out.println(e);
+                        }
+                    }
                 }
             }
         }
+        leser.close();
     }
 
     public static void meny(){
