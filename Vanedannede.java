a944cb95e92f098ed2bf4f519930af26d7a374b8
chiull <62013184+chiull@users.noreply.github.com>
2020-03-25 01:27:26 +0100

    Update Vanedannede.java

--- Vanedannede.java
+++ Vanedannede.java
@@ -1,4 +1,4 @@
-public class Vanedannede extends Legemiddel{
+class Vanedannede extends Legemiddel{
     int styrke;
 
     public Vanedannede(String n, Double p, Double v, int s){
@@ -7,6 +7,16 @@ public class Vanedannede extends Legemiddel{
     }
 
     public int hentVanedannedeStyrke(){
+        return styrke;
+    }
+
+    @Override
+    public String hentType(){
+        return "Vanedannede";
+    }
+
+    @Override
+    public int hentStyrke(){
         return styrke;
     }
 }
