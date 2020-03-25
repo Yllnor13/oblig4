03e381afbf09ff8eeacd065ae819969163197fe0
chiull <62013184+chiull@users.noreply.github.com>
2020-03-25 01:24:52 +0100

    Update HvitResept.java

--- HvitResept.java
+++ HvitResept.java
@@ -15,4 +15,9 @@ public class HvitResept extends Resept{
     public String toString(){
         return("\ntype: Hvit Resept" + "\nlegemiddel: " + leg.hentNavn() + " lege: " + doc.hentNavn() + " pris: " + pasientID + " reit: " + reit + " pris: " + prosAaBetale());
     }
+
+    @Override
+    public String resType(){
+        return "hvit";
+    }
 }
