d74ea67cdb01820c47730bd3cba1f8b86c7f2a84
chiull <62013184+chiull@users.noreply.github.com>
2020-03-25 01:27:04 +0100

    Update Vanlig.java

--- Vanlig.java
+++ Vanlig.java
@@ -1,5 +1,15 @@
-public class Vanlig extends Legemiddel{
+class Vanlig extends Legemiddel{
     public Vanlig(String n, Double p, Double v){
         super(n,p,v);
+    }
+
+    @Override
+    public String hentType(){
+        return "Vanlig";
+    }
+
+    @Override
+    public int hentStyrke(){
+        return 0;
     }
 }
